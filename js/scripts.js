5038869651839851f4fcf118c915077738c1b715
Amir D. Sattari <adsattari@gmail.com>
2020-12-04 05:20:44 -0800

    Add website :)

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const giveGratitude = () => {
+  console.log("Gratitude to God & All That Is.");
+  console.log("Thank You. Thank You. Thank You.");
+  console.log("I AM the possibility of Unity, Love, Truth, & Enlightenment for All That Is.");
+};
+
+giveGratitude();
+
+setInterval(giveGratitude, 7777);
